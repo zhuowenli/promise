78b744d07b05bda08802c01200a171266606b565
zhuowenli <zhuowenligg@gmail.com>
2020-07-14 08:21:43 +0800

    :sparkles: 添加 input 输入框

---
+++ web/services/symbol.ts
@@ -0,0 +1,8 @@
+/*
+ * Author: 卓文理
+ * Email: zhuowenligg@gmail.com
+ * Date: 2020-07-14 07:28:52
+ */
+
+export const NextForm = Symbol('nextForm');
+export const NextFormItem = Symbol('nextFormItem');
